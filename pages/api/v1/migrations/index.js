417c521ffe46b7b8026f35abcb2d412f2eca69dc
alfredo.holz.junior <alfredojrgasper@gmail.com>
2024-12-20 23:47:51 -0300

    aulas do dia 22 e 23

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,32 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+
+async function migrations(req, res) {
+  if (req.method === "GET") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+
+    res.status(200).json(migrations);
+  } else if (req.method === "POST") {
+    const migrations = await migrationRunner({
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: false,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+
+    res.status(200).json(migrations);
+  }
+
+  return res.status(405).end();
+}
+
+export default migrations;
---
+++ test/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,8 @@
+test("GET to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations");
+
+  const body = await response.json();
+
+  expect(response.status).toBe(200);
+  expect(Array.isArray(body)).toEqual(true);
+});
