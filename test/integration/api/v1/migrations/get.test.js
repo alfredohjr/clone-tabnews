6054c7267adeaf43a50412dd81389f19c5e298cb
Alfredo Holz Junior <alfredojrgasper@gmail.com>
2025-01-04 02:36:11 +0000

    test: aplicando melhores praticas na escrita dos testes

--- test/integration/api/v1/migrations/get.test.js
+++ test/integration/api/v1/migrations/get.test.js
@@ -5,11 +5,15 @@ beforeAll(async () => {
   await orchestrator.clearDatabase();
 });
 
-test("GET to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Running pending migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
 
-  const body = await response.json();
+      const body = await response.json();
 
-  expect(response.status).toBe(200);
-  expect(Array.isArray(body)).toEqual(true);
+      expect(response.status).toBe(200);
+      expect(Array.isArray(body)).toEqual(true);
+    });
+  });
 });
